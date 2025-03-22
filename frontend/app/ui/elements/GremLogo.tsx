38fe540fecbb10032e8b6f8c65e5a0e330a39043
OlenaKolyada <FunkyCorgi>
2025-03-22 18:30:43 +0100

    Add Grem logo asset

--- frontend/app/ui/elements/GremLogo.tsx
+++ frontend/app/ui/elements/GremLogo.tsx
@@ -1,18 +1,18 @@
 // app/ui/elements/GremLogo.tsx
 
+import Image from 'next/image';
 import Link from 'next/link';
-import { API_URL } from "@/app/lib/config/constants";
+import { API_URL } from "@/app/lib/config";
 
 export function GremLogo() {
     return (
         <div className={`flex flex-row items-center leading-none text-white`}>
-            <Link href="/">
-                <img
+            <Link href='/'>
+                <Image
                     src={`${API_URL}/uploads/images/assets/logo.png`}
                     alt="Grem Logo"
                     width={100}
                     height={69}
-                    style={{ objectFit: 'contain' }}
                 />
             </Link>
         </div>
