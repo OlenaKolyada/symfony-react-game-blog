4bbfd892f22d1015915bc229e180172845b61325
OlenaKolyada <FunkyCorgi>
2025-03-26 15:24:20 +0100

    Install Symfony bundles at container startup

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 # Установка зависимостей, если vendor не существует
 if [ ! -d ./vendor ]; then
     composer install --no-interaction
+    php bin/console assets:install --symlink
 fi
 
 # Запуск сервера Apache
