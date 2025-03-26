7045592300e9553aa976584e2a04eaac85712695
OlenaKolyada <FunkyCorgi>
2025-03-26 15:50:22 +0100

    Set www-data user for web server process

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -5,7 +5,6 @@ set -e
 if [ ! -d ./vendor ]; then
     composer install --no-interaction
     php bin/console assets:install --symlink
-    chown -R www-data:www-data ./var
 fi
 
 # Запуск сервера Apache
