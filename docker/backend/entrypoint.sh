12ddce5e6631da7d1125a7c4ff8c3e725c851690
OlenaKolyada <FunkyCorgi>
2025-03-26 15:44:35 +0100

    Fix file permissions in Docker container

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 if [ ! -d ./vendor ]; then
     composer install --no-interaction
     php bin/console assets:install --symlink
+    chown -R www-data:www-data ./var
 fi
 
 # Запуск сервера Apache
