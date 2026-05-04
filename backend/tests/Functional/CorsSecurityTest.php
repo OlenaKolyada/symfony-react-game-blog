74405de9b46d183b478a148024b4907323f99638
OlenaKolyada <olena.kolyada@proton.me>
2026-05-04 19:14:27 +0200

    Restrict credentialed CORS headers

---
+++ backend/tests/Functional/CorsSecurityTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Tests\Functional;
+
+final class CorsSecurityTest extends ApiTestCase
+{
+    public function testApiCorsPreflightDoesNotAllowArbitraryHeaders(): void
+    {
+        $this->client->request(
+            'OPTIONS',
+            '/api/profile',
+            [],
+            [],
+            [
+                'HTTP_ORIGIN' => 'http://localhost:3000',
+                'HTTP_ACCESS_CONTROL_REQUEST_METHOD' => 'GET',
+                'HTTP_ACCESS_CONTROL_REQUEST_HEADERS' => 'X-Exploit, Content-Type',
+            ]
+        );
+
+        self::assertResponseStatusCodeSame(400);
+        self::assertStringContainsString('Unauthorized header x-exploit', $this->client->getResponse()->getContent());
+    }
+
+    public function testApiCorsPreflightAllowsExpectedHeaders(): void
+    {
+        $this->client->request(
+            'OPTIONS',
+            '/api/profile',
+            [],
+            [],
+            [
+                'HTTP_ORIGIN' => 'http://localhost:3000',
+                'HTTP_ACCESS_CONTROL_REQUEST_METHOD' => 'GET',
+                'HTTP_ACCESS_CONTROL_REQUEST_HEADERS' => 'Content-Type, Authorization',
+            ]
+        );
+
+        self::assertResponseStatusCodeSame(200);
+
+        $allowedHeaders = $this->client->getResponse()->headers->get('Access-Control-Allow-Headers');
+
+        self::assertNotNull($allowedHeaders);
+        self::assertStringContainsString('content-type', $allowedHeaders);
+        self::assertStringContainsString('authorization', $allowedHeaders);
+    }
+}
